ae88ea944c5fb563755d1523db04881fa6a5ea68
Sean Holcomb <seanholcomb@gmail.com>
2020-05-02 15:34:22 -0700

    Make service into interface

--- src/main/java/holcomb/sean/springbootjokes/services/JokeService.java
+++ src/main/java/holcomb/sean/springbootjokes/services/JokeService.java
@@ -1,19 +1,7 @@
 package holcomb.sean.springbootjokes.services;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
-
-@Service
-public class JokeService {
-
-    private ChuckNorrisQuotes chuckNorrisQuotes;
-
-    public JokeService() {
-        chuckNorrisQuotes = new ChuckNorrisQuotes();
-    }
-
-    public String getJoke(){
-        return chuckNorrisQuotes.getRandomQuote();
-    }
+public interface JokeService {
+    String getJoke();
 }
