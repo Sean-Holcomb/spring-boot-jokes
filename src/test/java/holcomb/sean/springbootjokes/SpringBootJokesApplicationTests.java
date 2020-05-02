3db44d7d516eb4612ce57a06672d1c029885779a
Sean Holcomb <seanholcomb@gmail.com>
2020-05-02 11:46:30 -0700

    "initial commit"

---
+++ src/test/java/holcomb/sean/springbootjokes/SpringBootJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package holcomb.sean.springbootjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
