0bdff8da094937fa7adabc6408bacbd95c68309b
Sean Holcomb <seanholcomb@gmail.com>
2020-05-02 13:24:47 -0700

    First attempt at app. Is functional

---
+++ src/main/java/holcomb/sean/springbootjokes/services/JokeService.java
@@ -0,0 +1,19 @@
+package holcomb.sean.springbootjokes.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JokeService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeService() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    public String getJoke(){
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
